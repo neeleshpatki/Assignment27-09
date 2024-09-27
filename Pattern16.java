a488f1d131a1e0ab7e48a7d014e9206137afd0d3
neeleshpatki <neeleshpatki@gmail.com>
2024-09-28 01:04:59 +0530

    Add files via upload

--- Pattern16.java
+++ Pattern16.java
@@ -2,18 +2,19 @@ package patternprinting;
 
 public class Pattern16 {
 	public static void main(String[] args) {
-		int n = 5;
-		for (int i =n; i >= 1; i--) {
-			for (int k = 1; k <=n-i ; k++) {
+		int n = 6;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j < i; j++) {
 				System.out.print(" ");
 			}
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print("* ");
+
+			for (int j = i; j <= n; j++) {
+				System.out.print(j + " ");
 			}
+
 			System.out.println();
 		}
-
 	}
-
 }
